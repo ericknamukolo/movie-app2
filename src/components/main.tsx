45d7d7f3f69cde25651022835a8e7cafbe79fc34
Erick Namukolo <erickmndev@gmail.com>
2025-03-06 01:29:56 +0200

    extracted the watch box

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useState } from 'react';
+import { ReactNode } from 'react';
 
 function Main({ children }: { children: ReactNode }) {
   return <main className='main'>{children}</main>;
